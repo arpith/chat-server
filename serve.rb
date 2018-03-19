22b6f0cdfb34b559d90364b0e5b1855f1d72e0d2
Arpith Siromoney <arpith@feedreader.co>
2018-03-19 17:18:07 -0400

    send messages between children

--- serve.rb
+++ serve.rb
@@ -1,17 +1,16 @@
 require "socket"
 
 server = TCPServer.open(2626)
+puts "listening on 2626"
 parent_socket, child_socket = UNIXSocket.pair
 
 while session=server.accept
   fork do
-    request = session.gets
-    parent_socket.close
-    child_socket.send(request, 0)
-    from_parent = child_socket.recv(100)
-    session.puts from_parent
+    while request = session.gets
+      #child_socket.send(request, 0)
+      parent_socket.send(request, 0)
+      from_parent = child_socket.recv(100)
+      session.puts from_parent
+    end
   end
-  child_socket.close
-  parent_socket.send("sent from parent (#{$$})", 0)
-  from_child = parent_socket.recv(100)
 end
