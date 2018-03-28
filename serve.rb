b463f91502e222f9b916b27ce2a8e1e0dd315ca7
Arpith Siromoney <arpith@feedreader.co>
2018-03-27 21:27:31 -0400

    use list of clients

--- serve.rb
+++ serve.rb
@@ -4,13 +4,26 @@ server = TCPServer.open(2626)
 puts "listening on 2626"
 parent_socket, child_socket = UNIXSocket.pair
 
-while session=server.accept
+CLIENTS = []
+
+def send_message(message)
+  puts CLIENTS.size
+  CLIENTS.each do | client |
+    client.puts message
+  end
+end
+
+while client=server.accept
+  CLIENTS.push(client)
   fork do
-    while request = session.gets
+    while input = client.gets
+      child_socket.send(send_message(input)
       #child_socket.send(request, 0)
-      parent_socket.send(request, 0)
-      from_parent = child_socket.recv(100)
-      session.puts from_parent
+      #puts input_line
+      #parent_socket.send(input_line, 0)
+      #output_line = child_socket.recv(100)
+      #puts output_line
+      #session.puts output_line
     end
   end
 end
